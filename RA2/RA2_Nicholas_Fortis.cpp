be0402b6e68ad5d96fb0ea43f5d1cef45b02a795
Nick Fortis <fortisn7@gmail.com>
2023-01-27 16:45:35 -0500

    Completed RA2

--- RA2/RA2_Nicholas_Fortis.cpp
+++ RA2/RA2_Nicholas_Fortis.cpp
@@ -2,25 +2,46 @@
 #include <fstream>
 using namespace std;
 
-int main() {
-    string filename;
-    string line;
-    int lineCounter;
+int main(int argc, char *argv[]) {
+    string fileName, line; 
+    string longestCommentedLine = "";
+    string longestNonCommentedLine = "";
     ifstream fin;
-    cout << "Enter the name of a file to read from: \n" << endl;
-    cin >> filename;
-    fin.open(filename.c_str());
+    int linesCounter = 0;
+    int commentedLinesCounter = 0;
+
+    cout << "Enter the name of a file to read from:" << endl;
+    cin >> fileName;
+    cout << endl;
+    fin.open(fileName.c_str());
     if(!fin) {
-        cerr << "File cannot be opened " << filename << endl;
+        cerr << "File cannot be opened " << fileName << endl;
         exit(1);
     }
 
     while(fin.good()) {
         getline(fin, line);
-        cout << line.length() << "," << line[0] << endl;
-        lineCounter++;
+        linesCounter++;
+        if(line.substr(0,2) == "##" || line.substr(0,2) == "//") {
+            commentedLinesCounter++;
+            if(longestCommentedLine.length() < line.length()) {
+                longestCommentedLine = line;
+            }
+        }
+        else {
+            if(longestNonCommentedLine.length() < line.length()) {
+                longestNonCommentedLine = line;
+            }
+        }
     }
-    cout << "Total Number of Lines: " << lineCounter-1 << endl;
+
+    cout << "Total Number of Lines: " << linesCounter-1 << endl;
+    cout << "Number of commented lines: " << commentedLinesCounter << endl;
+    cout << "Maximum Length of commented lines: " << longestCommentedLine.length() << endl;
+    cout << "Maximum Length of non-commented lines: " << longestNonCommentedLine.length() << endl;
+    cout << "Commented line of maximum length: " << "\"" << longestCommentedLine << "\"" << endl;
+    cout << "Non-commented line of maximum length: " << "\"" << longestNonCommentedLine << "\"" << endl;
+    
     fin.close();
     return 0;
 }
