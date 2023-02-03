a18f56d1525c614e86fc773c156a38e1aba21589
Nick Fortis <fortisn7@gmail.com>
2023-02-02 20:17:00 -0500

    Finished RA3

--- RA3/RA3_Nicholas_Fortis.cpp
+++ RA3/RA3_Nicholas_Fortis.cpp
@@ -1,67 +1,72 @@
 #include <iostream>
 #include <fstream>
-#include <istringstream>
+#include <sstream>
 #include <cctype>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-    string fileName, line, flag;
+int main(int argc, char *argv[]) {
+    string fileName, line, flag, word;
     ifstream fin;
     int wordCounter = 0;
     int type1Counter = 0;
     int type2Counter = 0;
     
     if (argc < 2) {
-        cout << endl; //might not need
         cerr << "NO SPECIFIED INPUT FILE NAME." << endl;
         return 1;
     }
 
     if (argc < 3) {
-        flag = "-all";
+        flag = "";
     }
     else {
         flag = string(argv[2]);
     }
-    if (flag != "-all" || flag != "-type1" || flag != "-type2") {
-        cerr << "UNRECOGNIZED FLAG" << flag << endl;
+    if (flag != "" && flag != "-all" && flag != "-type1" && flag != "-type2") {
+        cerr << "UNRECOGNIZED FLAG " << flag << endl;
         return 1;
     }
 
     fileName = string(argv[1]);
     fin.open(fileName.c_str());
     if (!fin) {
-        cout << endl; //might not need
         cerr << "CANNOT OPEN THE FILE " << fileName << endl;
         return 1;
     }
 
     while (fin.good()) {
         getline(fin, line);
-        istringstream iss (line);
+        istringstream iss(line);
         while (iss >> word) {
-            
-            
+            if (word[0] == '$' && word.length() > 1 && isalpha(word[1]) && isalpha(word[word.length()-1])) {    
+                type1Counter++;
+            }
+            if (word[0] == '@' && word.length() > 1 && isalpha(word[1]) && isalpha(word[word.length()-1])) {                
+                type2Counter++;
+            }
             wordCounter++;
         }
     }
 
     if (wordCounter == 0) {
         cout << "File is empty." << endl;
+        return 1;
     }
-    if (flag == "-type1"){
-        cout << "Total number of words: " << wordCounter << endl;
+    if (flag == "-type1") {
         cout << "Number of Type 1 names: " << type1Counter << endl;
     }
-    else if (flag == "-type2"){
+    else if (flag == "-type2") {
+        cout << "Number of Type 2 names: " << type2Counter << endl;
+    }
+    else if (flag == "-all") {
         cout << "Total number of words: " << wordCounter << endl;
+        cout << "Number of Type 1 names: " << type1Counter << endl;
         cout << "Number of Type 2 names: " << type2Counter << endl;
     }
     else {
         cout << "Total number of words: " << wordCounter << endl;
-        cout << "Number of Type 1 names: " << type1Counter << endl;
-        cout << "Number of Type 2 names: " << type2Counter << endl;
+        cout << "Total number of Type 1 and Type 2 names: " << type1Counter+type2Counter << endl;
     }
 
     fin.close();
