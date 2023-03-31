0a05d01b509e073685ec63226ab0a024b31ff0e2
Nick Fortis <fortisn7@gmail.com>
2023-03-31 16:29:58 -0400

    wip pa2

--- PA2/parser.cpp
+++ PA2/parser.cpp
@@ -54,6 +54,7 @@ bool Prog(istream& in, int& line) {
 		ParseError(line, "Missing StmtList");
 		return false;
 	}
+	cout << "(DONE)" << endl;
 	return true;
 }//End of Prog
 
@@ -74,7 +75,7 @@ bool StmtList(istream& in, int& line) {
 		status = StmtList(in, line);
 		//cout << "after calling StmtList" << endl;
 	}
-	else if(tok.GetToken() == ERR) {
+	else if(tok == ERR) {
 		ParseError(line, "Unrecognized Input Pattern");
 		return false;
 	}
@@ -84,12 +85,30 @@ bool StmtList(istream& in, int& line) {
 	}
 	return status;
 
-
 }//End of StmtList
 
-//Stmt ::= AssignStme | WriteLnStmt | IfStmt
+//Stmt ::= AssignStmt | WriteLnStmt | IfStmt
 bool Stmt(istream& in, int& line) {
-	//TODO
+	bool status = false;
+	LexItem tok = Parser::GetNextToken(in, line);
+
+	if (tok == WRITELN) {
+		status = WritelnStmt(in, line);
+	}
+	else if (tok == IF) {
+		status = IfStmt(in, line);
+	}
+	else if (tok == NIDENT || tok == SIDENT) {
+		Parser::PushBackToken(tok);
+		status = AssignStmt(in, line);
+	}
+	else {
+		ParseError(line, "Missing AssignStmt, WriteLnStmt, or IfStmt");
+		return false;
+	}
+
+	return status;
+
 }//End of Stmt
 
 //WritelnStmt:= writeln (ExprList) 
@@ -99,7 +118,6 @@ bool WritelnStmt(istream& in, int& line) {
 	
 	tok = Parser::GetNextToken(in, line);
 	if(tok != LPAREN) {
-		
 		ParseError(line, "Missing Left Parenthesis of Writeln Statement");
 		return false;
 	}
@@ -113,7 +131,6 @@ bool WritelnStmt(istream& in, int& line) {
 	
 	tok = Parser::GetNextToken(in, line);
 	if(tok != RPAREN) {
-		
 		ParseError(line, "Missing Right Parenthesis of Writeln Statement");
 		return false;
 	}
@@ -124,17 +141,113 @@ bool WritelnStmt(istream& in, int& line) {
 
 //IfStmt ::= if (Expr) ‘{‘ StmtList ‘}’ [ else ‘{‘ StmtList ‘}’ ]
 bool IfStmt(istream& in, int& line) {
-	//TODO
+	bool status = false;
+	LexItem tok;
+
+	tok = Parser::GetNextToken(in, line);
+	if(tok != LPAREN) {
+		ParseError(line, "Missing Left Parethesis of If Statement");
+		return false;
+	}
+
+	bool ex = Expr(in, line);
+	if(!ex) {
+		ParseError(line, "Missing expression IfStmt");
+		return false;
+	}
+
+	tok = Parser::GetNextToken(in, line);
+	if(tok != RPAREN) {
+		ParseError(line, "Missing Right Parethesis of IfStmt");
+		return false;
+	}
+
+	tok = Parser::GetNextToken(in, line);
+	if(tok != LBRACES) {
+		ParseError(line, "Missing Left Brace of IfStmt");
+		return false;
+	}
+
+	bool SL = StmtList(in, line);
+	if(!SL) {
+		ParseError(line, "Missing StmtList in IfStmt");
+		return false;
+	}
+
+	tok = Parser::GetNextToken(in, line);
+	if(tok != RBRACES) {
+		ParseError(line, "Missing Right Brace of IfStmt");
+		return false;
+	}
+
+	tok = Parser::GetNextToken(in, line);
+	if(tok == ELSE) {
+		tok = Parser::GetNextToken(in, line);
+		if(tok != LBRACES) {
+			ParseError(line, "Missing Left Brace of ElseStmt in IfStmt");
+			return false;
+		}
+
+		bool SL = StmtList(in, line);
+		if(!SL) {
+			ParseError(line, "Missing StmtList in ElseStmt in IfStmt");
+			return false;
+		}
+
+		tok = Parser::GetNextToken(in, line);
+		if(tok != RBRACES) {
+			ParseError(line, "Missing Right Brace of ElseStmt in IfStmt");
+			return false;
+		}
+	}
+	else {
+		Parser::PushBackToken(tok);
+	}
+
+	return true;
 }//End of IfStmt
 
 //AssignStmt ::= Var = Expr
 bool AssignStmt(istream& in, int& line) {
-	//TODO
+	LexItem tok;
+	
+	bool var = Var(in, line);
+	if(!var) {
+		ParseError(line, "Missing Var in AssignStmt");
+		return false;
+	}
+
+	tok = Parser::GetNextToken(in, line);
+	if(tok != ASSOP) {
+		ParseError(line, "Missing ASSOP in AssignStmt");
+		return false;
+	}
+
+	bool expr = Expr(in, line);
+	if(!var) {
+		ParseError(line, "Missing Expr in AssignStmt");
+		return false;
+	}
+
+	return true;
 }//End of AssignStmt
 
 //Var ::= NIDENT | SIDENT
 bool Var(istream& in, int& line) {
-	//TODO
+	LexItem tok = Parser::GetNextToken(in, line);
+	if(tok == NIDENT) {
+		defVar[tok.GetLexeme()] = true;
+		return defVar[tok.GetLexeme()];
+	}
+	else if(tok == SIDENT) {
+		defVar[tok.GetLexeme()] = true;
+		return defVar[tok.GetLexeme()];
+	}
+	else {
+		ParseError(line, "Unknown variable type in Var");
+		return false;
+	}
+
 }//End of Var
 
 //ExprList:= Expr {,Expr}
@@ -154,7 +267,7 @@ bool ExprList(istream& in, int& line) {
 		status = ExprList(in, line);
 		//cout << "after calling ExprList" << endl;
 	}
-	else if(tok.GetToken() == ERR) {
+	else if(tok == ERR) {
 		ParseError(line, "Unrecognized Input Pattern");
 		cout << "(" << tok.GetLexeme() << ")" << endl;
 		return false;
@@ -198,5 +311,23 @@ bool UnaryExpr(istream& in, int& line) {
 
 //PrimaryExpr ::= IDENT | SIDENT | NIDENT | ICONST | RCONST | SCONST | (Expr)
 bool PrimaryExpr(istream& in, int& line, int sign) {
-	//TODO
+	LexItem tok = Parser::GetNextToken(in, line);
+	if(tok == LPAREN) {
+		if(Expr(in, line)) {
+			tok = Parser::GetNextToken(in, line);
+			if(tok != RPAREN) {
+				ParseError(line, "Missing ')' in (Expr)");
+				return false;
+			}
+		}
+	}
+
+	if((tok == SIDENT || tok == NIDENT) && !defVar[tok.GetLexeme()]) {
+		ParseError(line, "Using undefined error");
+	}
+
+	if(tok != IDENT & tok != SIDENT && tok != NIDENT && tok != ICONST && tok != RCONST && tok != SCONST && !Expr(in, line)) {
+		return false;
+	}
+	return true;
 }//End of PrimaryExpr
